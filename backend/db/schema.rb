1824f01dac2f6b10cad1935f71efa5e9fe4c6784
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-08 14:13:33 -0400

    add access and refresh token to user schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_163912) do
+ActiveRecord::Schema.define(version: 2021_06_08_181256) do
 
   create_table "users", force: :cascade do |t|
     t.integer "spotify_id"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_06_08_163912) do
     t.string "points"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "access_token"
+    t.string "refresh_token"
   end
 
 end
