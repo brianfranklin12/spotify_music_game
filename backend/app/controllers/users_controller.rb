d8c0442a36cfabd90e70dfc721cb0d5ae1979390
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-08 13:51:29 -0400

    add dotenv gem and environment variables

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def create
+    render json: { code: params[:code] }
+    body = {
+      grant_type: "authorization_code",
+      code: params[:code],
+      redirect_uri: ENV['REDIRECT_URI'],
+      client_id: ENV['CLIENT_ID'],
+      client_secret: ENV['CLIENT_SECRET']
+    }
+
+    auth_response = RestClient.post('https://accounts.spotify.com/api/token', body)
+    binding.irb
+  end
+
 end
