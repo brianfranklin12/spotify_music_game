76b3d4be605f6e3d8155a5c96e8621a68065b520
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-09 08:15:12 -0400

    add name to user model & schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_181256) do
+ActiveRecord::Schema.define(version: 2021_06_09_030310) do
 
   create_table "users", force: :cascade do |t|
     t.integer "spotify_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_181256) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "access_token"
     t.string "refresh_token"
+    t.string "name"
   end
 
 end
