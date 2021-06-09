dabf6308698bbed980b84e9d9a17610c00e23845
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-09 08:16:31 -0400

    add code variable to capture auth code from url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import Login from './Login'
+import Dashboard from './Dashboard'
+
+const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  return code ? <Dashboard code={code}/> : <Login />
 }
 
 export default App;
