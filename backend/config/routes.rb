01b8b2fa8520d0f6985cc5509b4fcd82dbea6cb6
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-09 08:14:32 -0400

    change route to expect post request to /login

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'auth/spotify/callback', to: 'users#create'
+  # get 'auth/spotify/callback', to: 'users#create'
+  post '/login', to: 'users#create'
+  get '/profile', to: 'users#profile'
 end
