4581a9d08d5ea7ccbd03a8144b2d4a510f2c6a9f
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-09 08:15:59 -0400

    change create method capture params as [:_json]

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
+  def profile
+    binding.irb
+    user = current_user
+    render json: user
+  end
+
   def create
     body = {
       grant_type: "authorization_code",
-      code: params[:code],
+      code: params[:_json],
       redirect_uri: ENV['REDIRECT_URI'],
       client_id: ENV['CLIENT_ID'],
       client_secret: ENV['CLIENT_SECRET']
@@ -20,14 +26,14 @@ class UsersController < ApplicationController
     user_response = RestClient.get("https://api.spotify.com/v1/me", header)
     user_params = JSON.parse(user_response.body)
     
-    @user = User.new(spotify_id: user_params["id"], avatar: user_params["images"][0]["url"], access_token: auth_params["access_token"], refresh_token: auth_params["refresh_token"])
+    @user = User.new(spotify_id: user_params["id"], name: user_params["display_name"], avatar: user_params["images"][0]["url"], access_token: auth_params["access_token"], refresh_token: auth_params["refresh_token"])
     if @user.save
       @token = encode_token(user_id: @user_id)
       render json: { user: @user, jwt: @token }, status: :created
     else
-      render json: { error: "failed to create user" }
+      render json: { error: @user.errors.full_messages }
     end
-    # redirect_to "http://localhost:3000/"
+    
   end
 
 end
