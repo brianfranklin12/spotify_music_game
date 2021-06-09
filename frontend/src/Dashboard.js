fc29680f25975a935abb0a0181cd8f0640cfe8ec
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-09 14:07:16 -0400

    dashboard will fetch user info and display username and avatar

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
 import useAuth from './useAuth';
+import FetchUserInfo from './FetchUserInfo';
 
 export default function Dashboard({code}) {
   const accessToken = useAuth(code)
+
+  const [name, setName] = useState();
+  const [avatar, setAvatar] = useState();
+
+  useEffect(() => {
+    if (!accessToken) return
+    FetchUserInfo(accessToken)
+    .then(res => res.json())
+    .then(data => {
+      setName(data.display_name)
+      setAvatar(data.images[0].url)
+    })
+  }, [accessToken])
+ 
+
   return (
-    <div>{code}</div>
+    <div>
+      <img src={avatar} />
+      <h1>Welcome, {name}</h1>
+    </div>
   )
 }
