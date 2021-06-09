61c90e54c41d4471de55e69925a7fe0576f655e4
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-09 08:23:38 -0400

    add .catch with a relocation and console.log err to post request in useAuth

--- frontend/src/useAuth.js
+++ frontend/src/useAuth.js
@@ -15,5 +15,9 @@ export default function useAuth(code) {
     })
     .then(res => res.json())
     .then(data => console.log(data))
+    .catch(err => {
+      console.error(err);
+      window.location = "/"
+    })
   }, [code])
 }
