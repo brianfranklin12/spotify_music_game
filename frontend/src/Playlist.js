4aa941b5766274f04a4cf0b023bc2317624a7329
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-11 10:59:25 -0400

    FetchPlaylistTracks fetches data and stores in state in playlist.js

--- frontend/src/Playlist.js
+++ frontend/src/Playlist.js
@@ -1,11 +1,19 @@
 import FetchPlaylistTracks from './FetchPlaylistTracks'
+import { useState } from 'react';
 
 export default function Playlist({accessToken, playlist}) {
+  const [tracks, setTracks] = useState('');
 
+  const handleClick = () => {
+    FetchPlaylistTracks(accessToken, playlist.id)
+    .then(data => setTracks(data.items))
+  }
+
+  console.log(tracks)
   return (
-    <div className="playlist_item">
+    <div className="playlist_item" onClick={handleClick} >
       <h3>{playlist.name}</h3>
-      <img src={playlist.images[0].url} alt={playlist.name} onClick={() => FetchPlaylistTracks(accessToken, playlist.id)} />
+      <img src={playlist.images[0].url} alt={playlist.name} />
     </div>
   )
 }
