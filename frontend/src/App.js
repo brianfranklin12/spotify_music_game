d6c9ce1c45016c2ef070356a0320f9a3f9b777c4
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-11 15:35:38 -0400

    make '/' route redirect to '/dashboard' when logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Login from './Login'
 import Dashboard from './Dashboard'
 import Game from './Game'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Auth from './Auth';
 
 const code = new URLSearchParams(window.location.search).get('code');
@@ -19,9 +19,10 @@ function App() {
         <Route path="/playlist/:id">
           <Game accessToken={accessToken} />
         </Route>
-        <Route path="/">
+        <Route path="/dashboard">
           <Dashboard accessToken={accessToken}/>
         </Route>
+        <Redirect exact from="/" to="/dashboard" />
       </Switch>
     </Router>
     : <Login />
