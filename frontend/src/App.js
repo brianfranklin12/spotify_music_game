9a2d70c1e3458682a491fd6c3bdb5fb07b979a00
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-11 15:12:36 -0400

    check for accessToken in local storage before running fetch for one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,17 @@ import Login from './Login'
 import Dashboard from './Dashboard'
 import Game from './Game'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import useAuth from './useAuth';
+import Auth from './Auth';
 
 const code = new URLSearchParams(window.location.search).get('code');
 
 
 function App() {
-  const accessToken = useAuth(code)
+
+  const accessToken = localStorage['accessToken'] || Auth(code)
 
   return (
-    code ? 
+    accessToken?
     <Router>
       <Switch>
         <Route path="/playlist/:id">
