c2199d6027a88f0c31194c0a77cdb939e2500aae
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-15 07:51:31 -0400

    remove access and refresh token from user model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_030310) do
+ActiveRecord::Schema.define(version: 2021_06_15_115048) do
 
   create_table "users", force: :cascade do |t|
     t.integer "spotify_id"
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 2021_06_09_030310) do
     t.string "points"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "access_token"
-    t.string "refresh_token"
     t.string "name"
   end
 
