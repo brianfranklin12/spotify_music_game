39a85d94ae99aaa4ad2385406d6ac274be18f157
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-16 10:35:20 -0400

    gamescontroller creates associated track items and returns them all as json data

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -8,10 +8,15 @@ class GamesController < ApplicationController
     playlist_response = RestClient.get("https://api.spotify.com/v1/playlists/#{params[:id]}/tracks", header)
     playlist_params = JSON.parse(playlist_response.body)
 
-    game = Game.new(user_id: current_user.id, playlist_uri: params[:id], points: 0)
+    game = Game.create(user_id: current_user.id, playlist_uri: params[:id], points: 0)
 
-    if game.save
-      render json: game
+    tracks = playlist_params["items"].each do |track|
+      new_track = Track.create(game_id: game.id, name: track["track"]["name"], artist: track["track"]["artists"][0]["name"], uri: track["track"]["uri"])
+    end
+
+
+    if game
+      render json: { game: game, tracks: game.tracks }
     else
       render json: { error: game.errors.full_messages }
     end
