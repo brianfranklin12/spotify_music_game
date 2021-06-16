f84e685a1c8ef20af4bbe9855961b32568187dd7
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-16 16:08:07 -0400

    add logic to generage questions and populate with wrong answers upon game creation

--- backend/app/controllers/games_controller.rb
+++ backend/app/controllers/games_controller.rb
@@ -14,12 +14,21 @@ class GamesController < ApplicationController
       new_track = Track.create(game_id: game.id, name: track["track"]["name"], artist: track["track"]["artists"][0]["name"], uri: track["track"]["uri"])
     end
 
+    questions = game.tracks.each do |track|
+      new_question = Question.create(game_id: game.id, track_uri: track.uri, correct_artist: track.artist)
+      wrong_artists = new_question.three_wrong_answers
+      new_question.a1 = wrong_artists[0]
+      new_question.a2 = wrong_artists[1]
+      new_question.a3 = wrong_artists[2]
+      new_question.save
+    end
 
     if game
-      render json: { game: game, tracks: game.tracks }
+      render json: { game: game, questions: game.questions }
     else
       render json: { error: game.errors.full_messages }
     end
     
   end
+
 end
