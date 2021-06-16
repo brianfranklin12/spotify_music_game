18671ee18999c1801e46768b8cd349a0e0c0c842
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-16 09:58:59 -0400

    rework game component to use dispatch NewGame instead of fetchplaylisttracks

--- frontend/src/pages/Game.js
+++ frontend/src/pages/Game.js
@@ -4,20 +4,27 @@ import { useParams } from 'react-router-dom';
 import Player from '../components/Player'
 import {ReactComponent as LeftIcon} from '../icons/chevron-left.svg';
 import { Link } from 'react-router-dom';
+import { NewGame } from '../services/NewGame';
+import { useDispatch } from 'react-redux';
 
 
 function Game({accessToken}) {
   const [tracks, setTracks] = useState([]);
   const { id } = useParams();
   const [currentTrack, setCurrentTrack] = useState('');
+  const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   FetchPlaylistTracks(accessToken, id)
+  //   .then(data => {
+  //     setTracks(data.items)
+  //     setCurrentTrack(data.items[0].track.uri)
+  //   })
+  // }, [accessToken, id])
 
   useEffect(() => {
-    FetchPlaylistTracks(accessToken, id)
-    .then(data => {
-      setTracks(data.items)
-      setCurrentTrack(data.items[0].track.uri)
-    })
-  }, [accessToken, id])
+    dispatch(NewGame({id, accessToken}))
+  }, [id])
 
   
   return (
@@ -26,7 +33,7 @@ function Game({accessToken}) {
       <div className="game-container">
         <h1>Game</h1>
         <Player accessToken={accessToken} uri={currentTrack} />
-        {tracks.map (track => {
+        {tracks && tracks.map (track => {
           const isActive = track.track.uri === currentTrack ? 'active' : null;
           return (
           <h4 
