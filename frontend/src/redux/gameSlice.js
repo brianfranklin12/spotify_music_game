56688be5605b69b45a0eef9b1193b28d30f11fc7
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-16 09:58:18 -0400

    set up extrareducer in gameSlice to save game info to state

--- frontend/src/redux/gameSlice.js
+++ frontend/src/redux/gameSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { NewGame } from '../services/NewGame';
 
 export const gameSlice = createSlice({
   name: "game",
@@ -7,6 +8,11 @@ export const gameSlice = createSlice({
     tracks: [],
     points: 0
   },
+  extraReducers: {
+    [NewGame.fulfilled] : (state, action) => {
+      console.log('made it back from rails with:', action.payload)
+    }
+  }
 })
 
 export default gameSlice.reducer;
