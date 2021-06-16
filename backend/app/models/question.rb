9cfdb16c815103713fb01544702741d9558a1b44
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-16 16:07:44 -0400

    create question model and methods to find wrong answers

---
+++ backend/app/models/question.rb
@@ -0,0 +1,18 @@
+class Question < ApplicationRecord
+  belongs_to :game
+
+  def artist_array
+    self.game.tracks.map do |track|
+      track.artist
+    end
+  end
+
+  def wrong_only
+    wrong_only = artist_array.select { |artist| artist != self.correct_artist }
+  end
+
+  def three_wrong_answers
+    wrong_only.sample(3)
+  end
+
+end
