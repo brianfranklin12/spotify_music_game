395b6f88a5660da554172b287637396f2408a02b
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-18 12:07:29 -0400

    save user info to global state

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,11 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { FetchUserInfo } from '../services/FetchUserInfo';
+import { SubmitPoints } from '../services/SubmitPoints';
 
 export const userSlice = createSlice({
   name: "user",
   initialState: {
+    id: null,
+    avatar: null,
+    name: null,
     accessToken: null,
-    jwt: null
+    points: null
   },
+  extraReducers: {
+    [FetchUserInfo.fulfilled]: (state, action) => {
+      console.log(action.payload)
+      state.id = action.payload.user.id
+      state.avatar = action.payload.user.avatar
+      state.name = action.payload.user.name
+      state.points = action.payload.user.points
+      localStorage.setItem('jwt', action.payload.jwt)
+    },
+    [SubmitPoints.fulfilled]: (state, action) => {
+      state.points = action.payload.points
+    }
+  }
 })
 
 export default userSlice.reducer;
