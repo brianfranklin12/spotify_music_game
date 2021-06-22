e4c257734b1ffb2ce0fca2bf78c84e2250e8d1b6
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-22 15:07:53 -0400

    change origin to only allow from frontend

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://spotify-music-game.netlify.app/'
 
     resource '*',
       headers: :any,
