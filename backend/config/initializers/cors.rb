32ba4f2cfa2e8b6f00f71441531bffdf6c3a274b
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-23 10:12:43 -0400

    cors origin now set by env variable

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://spotify-music-game.netlify.app/'
+    origins ENV['CORS_ORIGIN']
 
     resource '*',
       headers: :any,
